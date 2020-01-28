36f4d74c6a8273f94a356f0090b12d00123a0dc8
ayubharis <ayubh@mcmaster.ca>
2020-01-28 18:51:57 -0500

    Fixed .gitignore

--- apikeys.py
+++ apikeys.py
@@ -1,8 +1,3 @@
-import discord
-import requests
-import cassiopeia
-import json
-import spotipy
 TOKEN = 'NjI5MDI4OTM3MTU0MDM1NzI1.XZT3yg.t1-E53ZYdOq6IpGiKO2S_LUXuy8'
 CLIENT_ID = '290f920ccc774c03a536af4da9ff388d'
 CLIENT_SECRET ='f637f21cab23499797d0034895a01502'
